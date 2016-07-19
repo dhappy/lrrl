995ff11489e6b1b21a692b42ffa30c5746fa70a3
Will Holcomb <c_holcomb.william@cerner.com>
2016-07-18 22:08:43 -0500

    rails g scaffold book title:string author:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   resources :chapters
   resources :paragraphs
   # The priority is based upon order of creation: first created -> highest priority.
